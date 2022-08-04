be819a40311ec7bdaa0365ca3f6f45f3cb2e3ea0
khadijaMohammed <khadoj121998@gmail.com>
2022-08-04 00:04:58 -0700

    commit

---
+++ 03-datatypes/sample.php
@@ -0,0 +1,27 @@
+
+<?php
+/*
+String
+Integer
+Float (floating point numbers - also called double)
+Boolean
+Array
+Object
+NULL
+
+*/
+// int , float , string
+$int_var=1;
+$float_var=1.5;
+$str_var="x";
+# to print value and type
+var_dump($int_var);
+
+$cars = array("Volvo","BMW","Toyota");
+var_dump($cars);
+
+$x = "Hello world!";
+$x = null;
+var_dump($x);
+?>
+
